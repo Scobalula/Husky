51a9225283e522cce9baa6f71b19a1c5ff333b93
sheilan102 <33605593+sheilan102@users.noreply.github.com>
2022-06-01 14:32:46 +0300

    Modern Warfare 2019 (Parasyte) Support

--- src/Husky/Husky/GameStructures/Shared.cs
+++ src/Husky/Husky/GameStructures/Shared.cs
@@ -47,6 +47,23 @@ namespace Husky
     }
 
     /// <summary>
+    /// Gfx Vertex UV
+    /// </summary>
+    [StructLayout(LayoutKind.Sequential, Pack = 1)]
+    public unsafe struct GfxVertexUV
+    {
+        /// <summary>
+        /// U Texture Position
+        /// </summary>
+        public float U { get; set; }
+
+        /// <summary>
+        /// V Texture Position
+        /// </summary>
+        public float V { get; set; }
+    }
+
+    /// <summary>
     /// Gfx Vertex 
     /// </summary>
     [StructLayout(LayoutKind.Sequential, Pack = 1)]
@@ -227,5 +244,48 @@ namespace Husky
         /// End Bytes (Usage, etc.)
         /// </summary>
         public fixed byte Padding[0x14];
+    }
+
+    [StructLayout(LayoutKind.Sequential)]
+    public unsafe struct ParasyteXAsset64
+    {
+        public uint Type { get; set; }
+        public uint HeaderSize { get; set; }
+        public ulong ID { get; set; }
+        public uint Temp { get; set; }
+        public long Owner { get; set; }
+        public long Previous { get; set; }
+        public long Next { get; set; }
+        public long FirstChild { get; set; }
+        public long LastChild { get; set; }
+        public long Header { get; set; }
+        public long ExtendedDataSize { get; set; }
+        public long ExtendedData { get; set; }
+        public long ExtendedDataPtrOffset { get; set; }
+
+    }
+    [StructLayout(LayoutKind.Sequential)]
+    public unsafe struct ParasyteXAssetPool64
+    {
+        public long FirstXAsset { get; set; }
+        public long LastXAsset { get; set; }
+        public long LookupTable { get; set; }
+    }
+    
+    /// <summary>
+    /// 3f Vertex Position
+    /// </summary>
+    [StructLayout(LayoutKind.Sequential, Pack = 1)]
+    public unsafe struct GfxVertexPosition
+    {
+        public float X { get; set; }
+        public float Y { get; set; }
+        public float Z { get; set; }
+
+        public Vector3 ToCentimeter()
+        {
+            return new Vector3(X * 2.54, Y * 2.54, Z * 2.54);
+        }
+
     }
 }
